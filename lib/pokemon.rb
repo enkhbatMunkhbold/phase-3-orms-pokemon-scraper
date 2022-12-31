0d613aaccf83d19f2845f670733be0383c03c9e8
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-12-30 16:00:15 -0800

    Current Status

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,38 @@
-class Pokemon
+require "scraper.rb"
+require 'pry'
+
+class Pokemon < Scraper
+
+  attr_accessor :id, :name, :type, :db
+
+  def self.initialize (id=nil, name, type, db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+      INSERT INTO pokemon(name, type)
+      VALUES (?, ?)
+    SQL
+
+    db.execute(sql, name, type)
+    @id = db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+  end
+
+  def self.find(id, db)
+    sql = <<-SQL
+      SELECT * FROM pokemon
+      WHERE id = ?
+    SQL
+
+    result = db.execute(sql, [id])[0]
+    Pokemon.new(result[0], result[1], result[2])
+    # new_pokemon = Pokemon.new(pokemon_data[0], pokemon_data[1], pokemon_data[2])
+    # new_pokemon
+    # binding.pry
+  end
+
 end
